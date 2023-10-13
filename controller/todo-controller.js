7478d8a543ef8079caacc442591483f5b7409c6b
Enaholo Akhere <enaholoa@gmail.com>
2023-10-14 00:25:23 +0100

    :sparkles: feat(vercel): vercel hosting

--- controller/todo-controller.js
+++ controller/todo-controller.js
@@ -2,14 +2,17 @@ const todoServices = require('../services/todo_services');
 const express = require('express');
 const router = express.Router();
 
-router.post('/todos', todoServices.addTodo);
+const neon_url =
+  'postgres://Enaholo-Akhere:FIuoW19hDtSU@ep-steep-leaf-20820975-pooler.us-east-2.aws.neon.tech/neondb';
 
-router.get('/todos', todoServices.getTodos);
+router.post(`/todos`, todoServices.addTodo);
 
-router.get('/todos/:id', todoServices.getTodosByID);
+router.get(`/todos`, todoServices.getTodos);
 
-router.put('/todos/:id', todoServices.updateTodo);
+router.get(`/todos/:id`, todoServices.getTodosByID);
 
-router.delete('/todos/:id', todoServices.deleteTodo);
+router.put(`/todos/:id`, todoServices.updateTodo);
+
+router.delete(`/todos/:id`, todoServices.deleteTodo);
 
 module.exports = router;
--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'asdf',
+//   // host: 'localhost',
+//   host: 'ep-steep-leaf-20820975.us-east-2.aws.neon.tech',
+//   port: 5432,
+//   database: 'neondb',
+// });
+
 const pool = new Pool({
-  user: 'postgres',
-  password: 'asdf',
-  host: 'localhost',
-  port: 5432,
-  database: 'perntodo',
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 module.exports = pool;
